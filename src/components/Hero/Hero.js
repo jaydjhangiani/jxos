8f5161122de438d5a36f7ed545f064f674ba471d
Jay Jhangiani <jaydjhangiani@gmail.com>
2021-07-26 15:31:58 +0530

    completed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,21 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+ <Section row nopadding>
+   <LeftSection>
+     <SectionTitle main center>
+       Welcome to <br/>
+       JXOS Tech.
+     </SectionTitle>
+     <SectionText>
+     Digital Marketing and Web Development Start-Up established in June 2019. We offer consulting services in Social Media Management and develop pocket-friendly websites for small and medium-sized businesses.
+     </SectionText>
+     <Button onClick={() => window.location = "https://google.com"}>
+       Learn More
+     </Button>
+     
+   </LeftSection>
+ </Section>
 );
 
 export default Hero;
